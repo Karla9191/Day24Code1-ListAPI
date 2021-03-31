64778655e6582ddef4368ceb278e755912539596
Karla9191 <marietha2014@gmail.com>
2021-03-31 00:47:08 +0000

    new-change

--- src/main.py
+++ src/main.py
@@ -56,7 +56,7 @@ def get_todos():
 def add_todos():
 
     request_body = request.get_json()
-    todos = Todos(done=request_body["done"], label=request_body["label"])
+    todos = Todos(id=request_body["id"] ,done=request_body["done"], label=request_body["label"])
     db.session.add(todos)
     db.session.commit()
 
